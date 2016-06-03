b31c1b30e5cd9431f8b3b71e48491e7a7c9c6ecb
Amela Špica <amela.spica@gmail.com>
2016-06-03 20:42:19 +0200

    prikaz na index.php

--- detaljniPrikaz.php
+++ detaljniPrikaz.php
@@ -26,7 +26,7 @@
 		<div class='opisVremena'></div>
 		<div class='vrijeme'>" . $vijest['vrijeme'] . "</div>";
 		//prikaz autora
-		print "<h4 id='autor'>Autor: <a href='autorVijesti.php?autor=" . $autor['id'] . "'>" . $autor['naziv'] . "</a></h4>";
+		print "<h4 id='autor'>Autor: <a href='index.php?autor=" . $autor['id'] . "'>" . $autor['naziv'] . "</a></h4>";
 		print "<p>" . $vijest['opis'];
 		print "</p></article>";
 	?>
